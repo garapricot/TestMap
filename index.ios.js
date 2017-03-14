ebc4f47f995e8dad012769721ae57e8b580391e0
gareginhambardzumyan <yerevanrockcity@gmail.com>
2017-03-14 17:55:40 +0400

    Map Test

---
+++ index.ios.js
@@ -0,0 +1,10 @@
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import Index from './app/index'
+AppRegistry.registerComponent('TestMap', () => Index);
