6159f37704f3c07b1072684f67c89bed4ffc8a22
gareginhambardzumyan <yerevanrockcity@gmail.com>
2017-03-15 17:58:16 +0400

    Remake

--- app/index.js
+++ app/index.js
@@ -6,38 +6,37 @@ import {
   View,
   Dimensions
 } from 'react-native';
-import MapView from 'react-native-maps'
 const{width,height}=Dimensions.get('window')
-export default class Index extends Component {
+import MapView from 'react-native-maps';
+export default class Index extends Component{
   constructor(){
     super()
     this.state={
       region:{
-        latitude:null,
-        longitude:null,
-        latitudeDelta:null,
-        longitudeDelta:null
-        }
+       latitude: null,
+    longitude: null,
+    latitudeDelta: null,
+    longitudeDelta: null
       }
     }
-  
-calcDelta(lat,lon,accuracy){
-  const oneDegree0fLongitudInMeters=111.32;
-  const circumference=(40075/360)
-  const latDelta=accuracy*(1/(Math.cos(lat)*circumference))
-  const lonDelta=(accuracy/oneDegree0fLongitudInMeters)
-  this.setState({
-    region:{
+  }
+  calcDelta(lat,lon,accuracy){
+    const oneDegree0fLongitudInMeters=90
+    const circumference=(40075/360)
+    const latDelta=accuracy*(1/(Math.cos(lat)*circumference))
+    const lonDelta=(accuracy/oneDegree0fLongitudInMeters)
+    this.setState({
+      region:{
       latitude:lat,
       longitude:lon,
       latitudeDelta:latDelta,
       longitudeDelta:lonDelta
     }
-  })
-}
+    })
+  }
   componentWillMount(){
     navigator.geolocation.getCurrentPosition(
-    (position)=>{
+      (position)=>{
       const lat=position.coords.latitude
       const lon=position.coords.longitude
       const accuracy=position.coords.accuracy
@@ -46,24 +45,23 @@ calcDelta(lat,lon,accuracy){
     )
   }
   render(){
+    console.log(this.state.region.latitude)
     return(
     <View style={styles.container}>
-        {
-          this.state.region.lat? <MapView
-          style={styles.map}
-          initialRegion={this.state.region}
-          />:null
-        }
+       {this.state.region.latitude? <MapView
+                               style={styles.map}
+                               initialRegion={this.state.region}
+                               />:null}
       </View>
     );
   }
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+const styles=StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'#F5FCFF',
   },
   map:{
     flex:1,
